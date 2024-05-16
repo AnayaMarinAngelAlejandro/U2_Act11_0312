32bcc632f7dc4038fc128a46152e76a821120c2c
ANGEL_ALEJANDRO_ANAYA_MARIN <a.21308051280312@cbtis128.edu.mx>
2024-05-16 22:33:46 +0000

    Act2

---
+++ lib/widgets/health_needs.dart
@@ -0,0 +1,167 @@
+import 'package:flutter/material.dart';
+
+class HealthNeeds extends StatelessWidget {
+  const HealthNeeds({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    List<CustomIcon> customIcons = [
+      CustomIcon(name: "Eventos", icon: 'assets/eventos.jpg'),
+      CustomIcon(name: "Informacion", icon: 'assets/informacion.png'),
+      CustomIcon(name: "Ayuda", icon: 'assets/ayuda.png'),
+      CustomIcon(name: "Mas", icon: 'assets/more.png'),
+    ];
+    List<CustomIcon> healthNeeds = [
+      CustomIcon(name: "Eventos", icon: 'assets/eventos.jpg'),
+      CustomIcon(name: "Informacion", icon: 'assets/informacion.png'),
+      CustomIcon(name: "Ayuda", icon: 'assets/ayuda.png'),
+      CustomIcon(name: "WWE TV", icon: 'assets/wwetv.png'),
+    ];
+    List<CustomIcon> specialisedCared = [
+      CustomIcon(name: "Juguetes", icon: 'assets/juguetes.jpg'),
+      CustomIcon(name: "Ropa", icon: 'assets/ropa.jpg'),
+      CustomIcon(name: "Coleccioables", icon: 'assets/coleccionables.jpg'),
+      CustomIcon(name: "Boletos", icon: 'assets/boletos.jpg'),
+    ];
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: List.generate(customIcons.length, (index) {
+        return Column(
+          children: [
+            InkWell(
+              onTap: () {
+                if (index == customIcons.length - 1) {
+                  showModalBottomSheet(
+                    context: context,
+                    showDragHandle: true,
+                    builder: (context) {
+                      return Container(
+                        width: double.infinity,
+                        padding: const EdgeInsets.all(20),
+                        height: 350,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            // HEALTH NEEDS SECTION
+                            Text(
+                              "Principales",
+                              style: Theme.of(context).textTheme.titleLarge,
+                            ),
+                            const SizedBox(height: 10),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: List.generate(
+                                healthNeeds.length,
+                                (index) {
+                                  return Column(
+                                    children: [
+                                      InkWell(
+                                        onTap: () {},
+                                        borderRadius: BorderRadius.circular(90),
+                                        child: Container(
+                                          width: 60,
+                                          height: 50,
+                                          padding: const EdgeInsets.all(15),
+                                          decoration: BoxDecoration(
+                                            color: Theme.of(context)
+                                                .colorScheme
+                                                .primaryContainer
+                                                .withOpacity(0.4),
+                                            shape: BoxShape.circle,
+                                          ),
+                                          child: Image.asset(
+                                            healthNeeds[index].icon,
+                                          ),
+                                        ),
+                                      ),
+                                      const SizedBox(height: 6),
+                                      Text(healthNeeds[index].name)
+                                    ],
+                                  );
+                                },
+                              ),
+                            ),
+                            const SizedBox(height: 20),
+                            // SPECIALISED CARE SECTION
+
+                            Text(
+                              "Tienda Online",
+                              style: Theme.of(context).textTheme.titleLarge,
+                            ),
+                            const SizedBox(height: 15),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: List.generate(
+                                specialisedCared.length,
+                                (index) {
+                                  return Column(
+                                    children: [
+                                      InkWell(
+                                        onTap: () {},
+                                        borderRadius: BorderRadius.circular(90),
+                                        child: Container(
+                                          width: 60,
+                                          height: 60,
+                                          padding: const EdgeInsets.all(15),
+                                          decoration: BoxDecoration(
+                                            color: Theme.of(context)
+                                                .colorScheme
+                                                .primaryContainer
+                                                .withOpacity(0.4),
+                                            shape: BoxShape.circle,
+                                          ),
+                                          child: Image.asset(
+                                            specialisedCared[index].icon,
+                                          ),
+                                        ),
+                                      ),
+                                      const SizedBox(height: 6),
+                                      Text(specialisedCared[index].name)
+                                    ],
+                                  );
+                                },
+                              ),
+                            )
+                          ],
+                        ),
+                      );
+                    },
+                  );
+                }
+              },
+              borderRadius: BorderRadius.circular(90),
+              child: Container(
+                width: 60,
+                height: 60,
+                padding: const EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                  color: Theme.of(context)
+                      .colorScheme
+                      .primaryContainer
+                      .withOpacity(0.4),
+                  shape: BoxShape.circle,
+                ),
+                child: Image.asset(
+                  customIcons[index].icon,
+                ),
+              ),
+            ),
+            const SizedBox(height: 6),
+            Text(customIcons[index].name)
+          ],
+        );
+      }),
+    );
+  }
+}
+
+class CustomIcon {
+  final String name;
+  final String icon;
+
+  CustomIcon({
+    required this.name,
+    required this.icon,
+  });
+}
